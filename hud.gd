105b4332fb917b2877571a6da294d8b7b0ef2dd6
InsaneHarpy <dragon.jenna@gmail.com>
2023-11-25 14:34:57 -0700

    main menu view and working progress bar

--- hud.gd
+++ hud.gd
@@ -2,17 +2,20 @@ extends CanvasGroup
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Ebar_placeholder/Energy Bar").text = str(Global.player_energy) + " / 100"
+#	get_node("Ebar_placeholder/Energy Bar").text = str(Global.player_energy) + " / 100"
+	get_node("energy_bar").value = Global.player_energy
 	get_node("money/Monies").text = str(Global.player_money)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_node("Ebar_placeholder/Energy Bar").text = str(Global.player_energy) + " / 100"
+#	get_node("Ebar_placeholder/Energy Bar").text = str(Global.player_energy) + " / 100"
+	get_node("energy_bar").value = Global.player_energy
 	get_node("money/Monies").text = str(Global.player_money)
 	
 func update_energy(delta):
 	Global.player_energy += delta
-	get_node("Ebar_placeholder/Energy Bar").text = str(Global.player_energy) + " / 100"
+#	get_node("Ebar_placeholder/Energy Bar").text = str(Global.player_energy) + " / 100"
+	get_node("energy_bar").value = Global.player_energy
 	
 func update_money(delta):
 	Global.player_money += delta
